06c40db77eabec5d1cc6dd03397ae9fd74e54b89
Annie Zhou <a.zhou4847@gmail.com>
2022-12-13 21:31:23 -0500

    style/cleanup 2

--- NetworkManager.js
+++ NetworkManager.js
@@ -38,43 +38,6 @@ class NetworkManager {
     );
   }
 
-  //   getRandomQuote() {
-  //     console.log("Random quote called");
-  //     var category = "happiness";
-  //     let a = request.get(
-  //       {
-  //         url: "https://api.api-ninjas.com/v1/quotes?category=" + category,
-  //         headers: {
-  //           "X-Api-Key": process.env.API_KEY,
-  //         },
-  //       },
-  //       function (err, res, body) {
-  //         let errObj = {
-  //           quote: "No quotes found",
-  //           author: "N/A",
-  //         };
-
-  //         if (err || res.statusCode != 200) {
-  //           console.log("err:" + body.toString("utf8"));
-  //           return errObj;
-  //         } else {
-  //           console.log("Response received");
-  //           let obj = JSON.parse(body);
-  //           if (obj.length == 0) {
-  //             return errObj;
-  //           } else {
-  //             console.log(obj[0]);
-  //             return obj[0];
-  //           }
-  //         }
-  //       }
-  //     );
-
-  //     console.log("This is a " + a);
-
-  //     return a;
-  //   }
 }
 
 module.exports = NetworkManager;
-// exports = { NetworkManager };
